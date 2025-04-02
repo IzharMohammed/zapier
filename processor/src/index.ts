925857ef25eb7bb2baf4cc6dbb4df7db5e5f7e9d
MohammedIzhar77 <izharmohammed21@gmail.com>
2025-04-02 21:08:06 +0530

    Added kafka processor for picking up zaps

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,38 +1,44 @@
 import { Kafka } from "kafkajs"
+import { db } from "./db"
+import { zapRuns, zapRunsOutbox } from "./db/schema"
+import { eq, inArray, sql } from "drizzle-orm"
+
+const TOPIC_NAME = "zap-events"
 
 const kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: ['kafka1:9092', 'kafka2:9092']
+    clientId: 'processor',
+    brokers: ['localhost:9092']
 })
 
 const producer = kafka.producer()
-const consumer = kafka.consumer({ groupId: 'test-group' })
-
 
 async function main() {
+    await producer.connect()
     while (1) {
-        await producer.connect()
+
+        const pendingZapRuns = await db.select().from(zapRunsOutbox).limit(10);
+
+        if (pendingZapRuns.length === 0) {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            continue;
+        }
+
         await producer.send({
-            topic: 'test-topic',
-            messages: [
-                { value: 'Hello KafkaJS user!' },
-            ],
-        })
-
-        // Consuming
-        await consumer.connect()
-        await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
-
-        await consumer.run({
-            eachMessage: async ({ topic, partition, message }) => {
-                console.log({
-                    partition,
-                    offset: message.offset,
-                    value: message.value!.toString(),
+            topic: TOPIC_NAME,
+            messages: pendingZapRuns.map(zaps => ({
+                value: JSON.stringify({
+                    zapRunId: zaps.zapRunId
                 })
-            },
+            }))
         })
+
+        await db.delete(zapRunsOutbox).where(
+            inArray(
+                zapRunsOutbox.zapRunId,
+                pendingZapRuns.map(x => x.zapRunId)
+            )
+        )
     }
 }
 
-main()
+main().catch(console.error)
